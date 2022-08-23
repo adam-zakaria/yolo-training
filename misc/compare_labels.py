4aa4d5f8e8fbb97d5b7e914dad43ea77461a591c
Adam Zakaria <adam.p.zakaria@gmail.com>
2022-08-23 15:05:24 -0400

    Combine labels tested and working

--- misc/compare_labels.py
+++ misc/compare_labels.py
@@ -9,24 +9,25 @@ fs = os.listdir('/usr/src/datasets/nightowls_val/labels')
 for f in fs[:3]:
     filename_nightowls = f'{nightowls_labels_path}{f}'
     filename_yolo = f'{yolo_labels_path}{f}'
-    #print(f)
-    #print('Nightowls:')
+    print(f)
+    print('Nightowls:')
     with open(filename_nightowls, encoding='latin-1') as fx:
         num_ped_nightowls = 0
         ls = "" 
         for l in fx:
+            print(l)
             if combine_labels:
                 ls += l
             num_ped_nightowls += 1
         if f not in os.listdir(yolo_labels_path):
             ##print(f'{f} not in {yolo_labels_path}')
             continue 
-        ##print('--------')
+        print('--------')
         print('YOLO:')
         with open(filename_yolo, encoding='latin-1') as fy:
             num_ped_yolo = 0
             if combine_labels:
-                with open(f'{combined_labels_path}{f}','a' ,encoding='latin-1') as fz:
+                with open(f'{combined_labels_path}{f}','w' ,encoding='latin-1') as fz:
                     fz.write(ls)
             for l in fy:
                 print(l)
