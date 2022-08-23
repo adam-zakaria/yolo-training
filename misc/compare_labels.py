66356c58d6cf54d16ffb55403ab395eff9c85428
Adam Zakaria <adam.p.zakaria@gmail.com>
2022-08-23 14:55:41 -0400

    Combine labels seems to work

--- misc/compare_labels.py
+++ misc/compare_labels.py
@@ -6,33 +6,38 @@ yolo_labels_path = '/usr/src/yolov5/runs/detect/exp22/labels/'
 combined_labels_path = '/usr/src/datasets/combined_labels/'
 
 fs = os.listdir('/usr/src/datasets/nightowls_val/labels')
-for f in fs:
+for f in fs[:3]:
     filename_nightowls = f'{nightowls_labels_path}{f}'
     filename_yolo = f'{yolo_labels_path}{f}'
-    print(f)
+    #print(f)
     #print('Nightowls:')
     with open(filename_nightowls, encoding='latin-1') as fx:
         num_ped_nightowls = 0
+        ls = "" 
         for l in fx:
-            #print(l)
+            if combine_labels:
+                ls += l
             num_ped_nightowls += 1
-    if f not in os.listdir(yolo_labels_path):
-        #print(f'{f} not in {yolo_labels_path}')
-        continue 
-    #print('--------')
-    #print('YOLO:')
-    with open(filename_yolo, encoding='latin-1') as fy:
-        num_ped_yolo = 0
-        for l in fy:
-            #print(l)
-            category = int(l.split()[0])
-            if category == 0: #person
-                num_ped_yolo += 1
-            else:
-                if combine_labels:
-                    with open(f'{combined_labels_path}{f}','a' ,encoding='latin-1') as fz:
-                        fz.write(f'{l}')
-    if num_ped_nightowls <  num_ped_yolo:
-        print(f'num_ped_nightowls < num_ped_yolo: {num_ped_nightowls <  num_ped_yolo}')
-    ##print(f'num_ped_nightowls == num_ped_yolo: {num_ped_nightowls==num_ped_yolo}')
-    #print('------------------------------------')
+        if f not in os.listdir(yolo_labels_path):
+            ##print(f'{f} not in {yolo_labels_path}')
+            continue 
+        ##print('--------')
+        print('YOLO:')
+        with open(filename_yolo, encoding='latin-1') as fy:
+            num_ped_yolo = 0
+            if combine_labels:
+                with open(f'{combined_labels_path}{f}','a' ,encoding='latin-1') as fz:
+                    fz.write(ls)
+            for l in fy:
+                print(l)
+                category = int(l.split()[0])
+                if category == 0: #person
+                    num_ped_yolo += 1
+                else:
+                    if combine_labels:
+                        with open(f'{combined_labels_path}{f}','a' ,encoding='latin-1') as fz:
+                            fz.write(f'{l}')
+        #if num_ped_nightowls <  num_ped_yolo:
+            #print(f'num_ped_nightowls < num_ped_yolo: {num_ped_nightowls <  num_ped_yolo}')
+        ###print(f'num_ped_nightowls == num_ped_yolo: {num_ped_nightowls==num_ped_yolo}')
+        print('------------------------------------')
